ffa2ae035cd97bd2732fa5afcedf16e27c77079a
Gróf András <andrasgrof93@gmail.com>
2021-11-06 14:54:00 +0100

    can game drawing and switch select

---
+++ src/Game.java
@@ -0,0 +1,44 @@
+public class Game {
+
+    private boolean[][] grid;
+
+    public Game(int size) {
+        this.grid = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = false;
+            }
+        }
+    }
+
+    public void randomize() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (Math.random() > 0.53) {
+                    grid[i][j] = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j]) {
+                    s += "#";
+                } else {
+                    s += ".";
+                }
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    public void toggle(int x, int y) {
+        grid[x][y] = !grid[x][y];
+    }
+
+}
