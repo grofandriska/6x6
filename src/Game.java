ca3f66e36912b987220ef443a617bbf82d1a50f2
Gróf András <andrasgrof93@gmail.com>
2021-11-08 21:49:45 +0100

    ai improved

--- src/Game.java
+++ src/Game.java
@@ -16,7 +16,6 @@ public class Game {
 
     private List<int[]> bestSteps;
 
-
     public Game(int size) {
         init(size);
     }
@@ -49,9 +48,9 @@ public class Game {
     }
 
     public boolean isSolved() {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j]) {
+        for (int i = 0; i < copy.length; i++) {
+            for (int j = 0; j < copy[i].length; j++) {
+                if (copy[i][j]) {
                     return false;
                 }
             }
@@ -67,7 +66,7 @@ public class Game {
         }
     }
 
-    public void toggleAi(int x, int y) {
+    public void toggleCell(int x, int y) {
         copy[x][y] = !copy[x][y];
 
         if (x < copy.length - 1) {
@@ -82,74 +81,87 @@ public class Game {
         if (y > 0) {
             copy[x][y - 1] = !copy[x][y - 1];
         }
-        steps.add(new int[]{x, y});
-    }
-
-    public void toggle(int x, int y) {
-        grid[x][y] = !grid[x][y];
-        if (x < grid.length - 1) {
-            grid[x + 1][y] = !grid[x + 1][y];
-        }
-        if (x > 0) {
-            grid[x - 1][y] = !grid[x - 1][y];
-        }
-
-        if (y < grid.length - 1) {
-            grid[x][y + 1] = !grid[x][y + 1];
-        }
-        if (y > 0) {
-            grid[x][y - 1] = !grid[x][y - 1];
-        }
+        steps.add(new int[] { x, y });
     }
 
     public void chasingTheLights() {
         for (int i = 1; i < copy.length; i++) {
             for (int j = 0; j < copy[i].length; j++) {
                 if (copy[i - 1][j] = true) {
-                    toggleAi(i, j);
+                    toggleCell(i, j);
                 }
             }
         }
     }
 
     public void calculateSteps() {
-        int XX = 0;
+        int indexMarker = 0; // --> ez volt az "xx" 🙂
         int[] counters = new int[width];
 
-        //setting counters acc to width
+        // setting counters acc to width
         for (int i = 0; i < counters.length; i++) {
             counters[i] = i;
         }
 
-        //saving best moves
-        for (int i = 0; i < XX; i++) {
+        int currentIndex = 0;
+        do {
             steps.clear();
-            toggleAi(i, 0);
-
+            copyBoard();
+            for (int i = 0; i <= indexMarker; i++) { // toggle bottom lights per current variation of counters
+                toggleCell(0, counters[i]);
+            }
             chasingTheLights();
-
+            // checking state after Chasing of Ligths and if it is correct state
+            // and this is a best move then store the steps
             if (isSolved()) {
                 if (steps.size() < minSteps) {
                     minSteps = steps.size();
                     bestSteps = List.copyOf(steps);
                 }
-
             }
-            if (counters[i] + 1 < counters[i + 1]) {
-                counters[i]++;
-            }else{
 
-            }
-        }
+            // stepping the counters
+            boolean successed = false;
+            do {
+                if (currentIndex == indexMarker) {
+                    if (counters[currentIndex] < this.width - 1) {
+                        counters[currentIndex]++;
+                        successed = true;
+                        currentIndex = 0;
+                    } else {
+                        counters[currentIndex] = currentIndex;
+                        for (int i = 0; i < indexMarker; i++) {
+                            counters[i] = i;
+                        }
+                        currentIndex = 0;
+                        indexMarker++;
+                        successed = true;
+                    }
+                } else if (counters[currentIndex] + 1 < counters[currentIndex + 1]) {
+                    counters[currentIndex]++;
+                    successed = true;
+                    currentIndex = 0;
+                } else {
+                    counters[currentIndex] = currentIndex;
+                    currentIndex++;
+                    successed = false;
+                }
+            } while (!successed);
+
+        } while (indexMarker < width);
+
     }
 
+    public List<int[]> getBestSteps() {
+        return bestSteps;
+    }
 
     @Override
     public String toString() {
         String s = "";
-        for (int i = 0; i < copy.length; i++) {
-            for (int j = 0; j < copy[i].length; j++) {
-                if (copy[i][j]) {
+        for (int y = 0; y < copy.length; y++) {
+            for (int x = 0; x < copy[y].length; x++) {
+                if (copy[y][x]) {
                     s += "#";
                 } else {
                     s += ".";
@@ -164,4 +176,3 @@ public class Game {
         return grid;
     }
 }
-
--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,7 @@ public class Main {
         System.out.println(game);
 
         Scanner scanner = new Scanner(System.in);
-
-
-
-            game.chasingTheLights();
-           /* try {
+          try {
                 System.out.println(game);
                 System.out.println("X ?");
                 int x = scanner.nextInt();
@@ -22,14 +18,14 @@ public class Main {
                 System.out.println("Y ?");
                 int y = scanner.nextInt();
 
-                game.toggle(x, y);
+                game.toggleCell(x, y);
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Please use a right number between 0 and " + (game.getGrid().length - 1));
             } catch (InputMismatchException e) {
                 System.out.println("Please use a right number between 0 and " + (game.getGrid().length - 1));
                 scanner.next();
                 scanner.close();
-            }*/
+            }
 
 
     }
